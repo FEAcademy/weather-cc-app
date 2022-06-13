c0f23a9d620a214679293ea5fb28869df36646ef
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-13 15:14:29 +0200

    Added Footer component Added StyledFooter component with styles

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { StyledFooter } from './StyledFooter'
+
+const Footer = () => {
+  return (
+    <StyledFooter><span>WEATHER</span>ly - <span>2022</span></StyledFooter>
+  )
+}
+
+export { Footer }
---
+++ src/components/Footer/StyledFooter.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 76px;
+  background-color: transparent;
+  color: #707070;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > span {
+    font-weight: bold;
+  }
+`
+
+export { StyledFooter }
