3d93f20410d015ca98c0f069364fc9a58d600374
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-13 17:21:09 +0200

    Added Navbar component Added navbarstyled components

---
+++ src/components/Navbar/Navbar.styled.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const NavContainer = styled.nav`
+    width: 100%;
+    height: 68px;
+    background: rgba(255, 255, 255, 0.9);
+    position: sticky;
+    top: 0;
+    display: flex;
+    align-items: center;
+    //justify-content: space-evenly;
+    padding-left: 100px; //just for now because AC requires logo only.
+    box-shadow: 0px 12px 20px rgba(0, 0, 0, 0.1);
+`
+const LogoTxt = styled.p`
+  color: #707070;
+  font-size: 18px;
+  font-weight: 300;
+  > span {
+    font-weight: 800;
+  }
+`
+
+export { NavContainer, LogoTxt }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { LogoTxt, NavContainer } from './Navbar.styled'
+
+const Navbar = () => {
+    return (
+        <NavContainer>
+            <LogoTxt><span>WEATHER</span>ly</LogoTxt>
+        </NavContainer>
+    )
+}
+
+export { Navbar }
