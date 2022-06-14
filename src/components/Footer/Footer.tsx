860415f3730202b3a4d793c4aeb21b9262cff583
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-14 10:49:59 +0200

    changed footer utils to Footer file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
-import { getCurrentYear } from 'utils/getCurrentYear'
+import { getCurrentYear } from 'components/Footer/utils/getCurrentYear'
 import { StyledFooter } from './StyledFooter'
 
 const Footer = () => {
   return (
     <StyledFooter>
-      Weather<span>ly</span>&nbsp; - &nbsp;{getCurrentYear()}</StyledFooter>
+      Weather<span>ly</span>&nbsp; - &nbsp;{getCurrentYear()}
+    </StyledFooter>
   )
 }
 
