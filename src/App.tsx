fc44148f9cf97905476ecdc42cc696580e171881
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-14 11:12:02 +0200

    Created styled Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Footer } from 'components/Footer'
 import { HomePage } from 'pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from './Container.styled'
 
 import 'assets/styles/index.css'
 
 const App = () => {
   return (
-    <>
+    <Container>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
       <Footer />
-    </>
+    </Container>
   )
 }
 
---
+++ src/Container.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    position: relative;
+    padding-bottom: 80px;
+    min-height: 100vh;
+`
+
+export { Container }
