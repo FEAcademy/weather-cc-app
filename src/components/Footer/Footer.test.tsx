e560f0182b5141dd86fac86c36425022e0b00b24
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-06-14 17:36:37 +0200

    organized tests with describe

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,27 +1,28 @@
 import { render, screen } from '@testing-library/react'
-import React from 'react'
 import { Footer } from './Footer'
 
-test('Footer renders', () => {
-  render(<Footer />)
+describe('Footer tests', () => {
+  test('Footer renders', () => {
+    render(<Footer />)
 
-  const footer = screen.getByTestId('Footer')
-  expect(footer).toBeInTheDocument()
-})
+    const footer = screen.getByTestId('Footer')
+    expect(footer).toBeInTheDocument()
+  })
 
-test('Footer not empty', () => {
-  render(<Footer />)
+  test('Footer not empty', () => {
+    render(<Footer />)
 
-  const footer = screen.getByTestId('Footer')
+    const footer = screen.getByTestId('Footer')
 
-  expect(footer).not.toBeEmptyDOMElement()
-})
+    expect(footer).not.toBeEmptyDOMElement()
+  })
 
-test('Footer displays date corectly', () => {
-  render(<Footer />)
+  test('Footer displays date corectly', () => {
+    render(<Footer />)
 
-  const currentYear = new Date().getFullYear()
-  const footer = screen.getByTestId('Footer')
+    const currentYear = new Date().getFullYear()
+    const footer = screen.getByTestId('Footer')
 
-  expect(footer).toHaveTextContent('WEATHERLY - ' + currentYear)
+    expect(footer).toHaveTextContent('WEATHERLY - ' + currentYear)
+  })
 })
