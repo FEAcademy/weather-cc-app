26b8a746dc1f76f381323b0528bc1c0a0e4e07e5
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-06-15 12:17:25 +0200

    Update tests

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,29 +4,20 @@ import { Footer } from './Footer'
 describe('Footer', () => {
   test('renders footer', () => {
     render(<Footer />)
-
     const footerText = screen.getByTestId('footer-text')
     expect(footerText).toBeVisible()
   })
 
-  test('footer is not empty', () => {
-    render(<Footer />)
-
-    const footerText = screen.getByTestId('footer-text')
-    expect(footerText).not.toBeEmptyDOMElement()
-  })
-
   test('renders footer text', () => {
     render(<Footer />)
-
     const footerText = screen.getByTestId('footer-text')
     expect(footerText).toHaveTextContent(/weatherly/i)
   })
 
   test('renders logo and current year', () => {
     render(<Footer />)
-
-    const currYear = new Date().getFullYear()
+    const now = jest.getRealSystemTime()
+    const currYear = new Date(now).getFullYear()
     const footerText = screen.getByTestId('footer-text')
     expect(footerText).toHaveTextContent(`WEATHERLY - ${currYear}`)
   })
