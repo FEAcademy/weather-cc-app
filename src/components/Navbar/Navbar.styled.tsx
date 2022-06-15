842acdab826cadd82192eb0f12236411727d21d0
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-06-15 13:09:27 +0200

    fixed container

--- src/components/Navbar/Navbar.styled.tsx
+++ src/components/Navbar/Navbar.styled.tsx
@@ -2,22 +2,17 @@ import styled from 'styled-components'
 
 export const Container = styled.nav`
   background: rgba(255, 255, 255, 0.9);
-  box-shadow: 0px 12px 20px rgba(0, 0, 0, 0.1);
   height: 60px;
   width: 100%;
   padding: 0 18px;
   display: flex;
   align-items: center;
+  justify-content: center;
 `
 
 export const Wrapper = styled.div`
   width: 100%;
   max-width: 1200px;
-  display: flex;
-  align-items: center;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
 `
 
 export const Logo = styled.h1`
