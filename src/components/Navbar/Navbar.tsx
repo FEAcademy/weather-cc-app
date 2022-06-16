639eb499c8da1225c3edf08be3da1cf82c855419
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-06-16 18:22:24 +0200

    changed to arrow function

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,11 @@
 import { Logo, Wrapper, Container } from './Navbar.styled';
 
-export function Navbar() {
-  return (
-    <Container>
-      <Wrapper>
-        <Logo>
-          Weather<span>Ly</span>
-        </Logo>
-      </Wrapper>
-    </Container>
-  );
-}
+export const Navbar = () => (
+  <Container>
+    <Wrapper>
+      <Logo>
+        Weather<span>Ly</span>
+      </Logo>
+    </Wrapper>
+  </Container>
+);
