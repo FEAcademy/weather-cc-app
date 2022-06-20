be29a540080a08eb9b25f20c740aa8520fbd195d
Kamil Borecki CC <95024445+kamil-borecki-cc@users.noreply.github.com>
2022-06-20 13:46:59 +0200

    added path groups for aliases in eslint (#16)
    * added path groups for aliases in eslint
    
    * remove redundant emptyline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { HomePage } from 'pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from 'pages/Home';
 
 import 'assets/styles/index.css';
 
