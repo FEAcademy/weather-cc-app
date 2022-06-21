5ac910366cb20ee478b8d583cdf7f458c23a9898
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-21 14:39:46 +0200

    little modification in footer.test.tsx

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -2,16 +2,15 @@ import { render, screen } from '@testing-library/react';
 import { Footer } from './Footer';
 
 describe('Footer Component', () => {
-    beforeAll(() => {
-        jest.useFakeTimers();
-        jest.setSystemTime(new Date('2022-01-01'));
-    });
     it('should render footer', () => {
         render(<Footer />);
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toBeInTheDocument();
     });
     it('should check if footer contains logo text with current year: "WEATHERLY - 2022"', () => {
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date('2022-01-01'));
+
         render(<Footer />);
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toHaveTextContent(/WEATHERLY - 2022/i);
