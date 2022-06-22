30cf0a576a9484385b1f860e6bb5960351302186
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-06-22 13:53:37 +0200

    Create basic container

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from './components/Container';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { HomePage } from './pages/Home';
@@ -7,11 +8,13 @@ import 'assets/styles/index.css';
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Footer />
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </Container>
     </Router>
   );
 };
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-height: 100vh;
+  position: relative;
+`;
+
+export { Container };
