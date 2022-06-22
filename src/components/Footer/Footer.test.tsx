a390f090114d6d0274c2258937441f84c231660a
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-22 12:37:51 +0200

    test description title corrected

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -7,7 +7,7 @@ describe('Footer Component', () => {
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toBeInTheDocument();
     });
-    it('should check if footer contains logo text with current year: "WEATHERLY - 2012"', () => {
+    it('should display logo text with current year: "WEATHERLY - 2012"', () => {
         jest.useFakeTimers();
         jest.setSystemTime(new Date('2012-01-01'));
 
