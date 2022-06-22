ccb0e3b470f0c920923b5fe9de39b3caeec358bd
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-06-22 12:57:53 +0200

    Change function implementation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,12 @@ import { Container, FooterText } from './Footer.styled';
 import { FooterTestIds } from './FooterTestIds';
 
 const Footer = () => {
-  const now = new Date();
-  const currYear = now.getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <Container data-testid={FooterTestIds.Footer}>
       <FooterText>
         WEATHER<span>LY</span>
-        <span>&nbsp;-&nbsp;{currYear}</span>
+        <span>&nbsp;-&nbsp;{currentYear}</span>
       </FooterText>
     </Container>
   );
