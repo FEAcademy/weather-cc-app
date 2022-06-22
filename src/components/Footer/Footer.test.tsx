058e37732a943fe070a4a634e5ff84f2016ba237
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-22 11:10:02 +0200

    change fake year from future to year from the past

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -7,12 +7,12 @@ describe('Footer Component', () => {
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toBeInTheDocument();
     });
-    it('should check if footer contains logo text with current year: "WEATHERLY - 2029"', () => {
+    it('should check if footer contains logo text with current year: "WEATHERLY - 2012"', () => {
         jest.useFakeTimers();
-        jest.setSystemTime(new Date('2029-01-01'));
+        jest.setSystemTime(new Date('2012-01-01'));
 
         render(<Footer />);
         const footerElement = screen.getByRole('contentinfo');
-        expect(footerElement).toHaveTextContent(/WEATHERLY - 2029/i);
+        expect(footerElement).toHaveTextContent(/WEATHERLY - 2012/i);
     });
 });
