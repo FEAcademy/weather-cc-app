78b8b11af50abd7607ab67e542bbbf482fa957dd
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-22 12:45:47 +0200

    changed code structure in the test file

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,15 +4,13 @@ import { Footer } from './Footer';
 describe('Footer Component', () => {
     it('should render footer', () => {
         render(<Footer />);
-        const footerElement = screen.getByRole('contentinfo');
-        expect(footerElement).toBeInTheDocument();
+        expect(screen.getByRole('contentinfo')).toBeInTheDocument();
     });
     it('should display logo text with current year: "WEATHERLY - 2012"', () => {
         jest.useFakeTimers();
         jest.setSystemTime(new Date('2012-01-01'));
 
         render(<Footer />);
-        const footerElement = screen.getByRole('contentinfo');
-        expect(footerElement).toHaveTextContent(/WEATHERLY - 2012/i);
+        expect(screen.getByRole('contentinfo')).toHaveTextContent(/WEATHERLY - 2012/i);
     });
 });
