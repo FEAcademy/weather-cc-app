56b24d868d479f618ebe5da7318aba17b6c83e3c
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-22 11:07:25 +0200

    tests fakeTime year changed, removing currentYear logic from utils to component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
-import { getCurrentYear } from 'components/Footer/utils/getCurrentYear';
 import { FooterText, StyledFooter } from './Footer.styled';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <StyledFooter>
       <FooterText>
         Weather
         <span>ly</span>
         <span>
-          &nbsp; - &nbsp;{getCurrentYear()}
+          &nbsp; - &nbsp;{currentYear}
         </span>
       </FooterText>
     </StyledFooter>
--- src/components/Footer/utils/getCurrentYear.ts
+++
@@ -1,6 +0,0 @@
-const getCurrentYear = (): number => {
-    const currentYear = new Date().getFullYear();
-    return currentYear;
-};
-
-export { getCurrentYear };
