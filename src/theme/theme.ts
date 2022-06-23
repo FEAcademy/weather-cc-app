cfdda5d21ccff7798dfd437bf6eaede1c27ee9e8
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-23 15:08:16 +0200

    applied YAGNI rule for creating theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,20 +5,6 @@ const theme = {
         maxWidthLayout: '1200px',
         paddingLayout: '0 18px',
     },
-    aqiWidget: {
-        backgroundColor: '#A9E0A8',
-        boxShadow: '0px 10px 30px #E3E3E3',
-        borderRadius: '20px'
-    },
-    mainTemperatureWidget: {
-        topFontSize: '20px',
-        mainFontSize: '48px',
-        bottomFontSize: '15px'
-    },
-    textSize: {
-        smf: '12px',
-        lgf: '20px',
-    }
 };
 
 export { theme };
