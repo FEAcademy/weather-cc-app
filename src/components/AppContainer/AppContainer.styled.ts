2a43fb6b8fcf6a2bd96bc536380144450ce3016f
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-06-27 14:51:25 +0200

    Change semantic in AppContainer

--- src/components/AppContainer/AppContainer.styled.ts
+++ src/components/AppContainer/AppContainer.styled.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Container = styled.main`
   flex: 1;
   display: flex;
   justify-content: center;
 `;
 
-const Wrapper = styled.main`
+const Wrapper = styled.div`
   width: 100%;
   max-width: ${(props) => props.theme.appMaxWidth};
 `;
