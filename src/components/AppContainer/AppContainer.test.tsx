a4155c5a82bf28a4df22fe72d0fc2cf7a20c935d
AgataLudwiczynska-CC <a.ludwiczynska@clearcode.cc>
2022-06-27 15:22:23 +0200

    FEA-98-Add tests to AppContainer

---
+++ src/components/AppContainer/AppContainer.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from 'theme/test-utils';
+import { AppContainer } from './AppContainer';
+
+describe('App Container', () => {
+  it('should render main container', () => {
+    render(<AppContainer />);
+    const main = screen.getByRole('main');
+    expect(main).toBeInTheDocument();
+  });
+  it('should render children', () => {
+    render(
+      <AppContainer>
+        <p data-testid={'children'}></p>
+      </AppContainer>,
+    );
+    const containerChild = screen.getByTestId('children');
+    expect(containerChild).toBeInTheDocument();
+  });
+});
