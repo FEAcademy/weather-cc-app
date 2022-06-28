a32e131a45de881409d2d601d4d1b01798af8741
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-06-28 14:21:25 +0200

    chenge theme-test render function name

--- src/theme/theme-test-utils.tsx
+++ src/theme/theme-test-utils.tsx
@@ -2,10 +2,10 @@ import { render, RenderOptions } from '@testing-library/react';
 import { ReactElement } from 'react';
 import { ThemeProvider } from './ThemeProvider';
 
-const customThemeRender = (
+const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
 ) => render(ui, { wrapper: ThemeProvider, ...options });
 
 export * from '@testing-library/react';
-export { customThemeRender as render };
+export { customRender as render };
