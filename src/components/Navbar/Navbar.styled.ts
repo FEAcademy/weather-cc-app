30b7d38fe8f665e3881c127316f9c34838b7a25e
lukasz-konatowski-cc <107026487+lukasz-konatowski-cc@users.noreply.github.com>
2022-06-29 14:07:20 +0200

    FEA-1 - Create core layout

---
+++ src/components/Navbar/Navbar.styled.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Container = styled.nav`
+  width: 100%;
+  height: 60px;
+  position: sticky;
+  display: flex;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.background};
+  opacity: 0.9;
+  padding: ${({ theme }) => theme.paddings.app};
+`;
+
+const Wrapper = styled.div`
+  max-width: ${({ theme }) => theme.widths.app};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  font-size: 18px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.colors.text};
+  text-transform: uppercase;
+
+  & span {
+    font-weight: 300;
+  }
+`;
+
+export { Logo, Wrapper, Container };
