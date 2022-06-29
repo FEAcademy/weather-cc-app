ff2390258286255e9ed83c7a35181d68fc6dc072
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-06-29 15:55:20 +0200

    Setup axios

---
+++ src/utils/axiosClient.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { IWeatherData } from './IWeatherData';
+
+const apiClient = axios.create({
+  baseURL: `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&aqi=yes&q=`,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+const getWeatherInCity = async (city: string) => {
+  const response = await apiClient.get<IWeatherData[]>(city);
+  return response.data;
+};
+
+export { getWeatherInCity };
