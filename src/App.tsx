c1f0cf45bf666d555e02b567c6217b0a169e3cba
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-06-30 11:15:04 +0200

    Delete some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { getWeatherInCity } from 'utils/axiosClient';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { PageContainer } from './components/PageContainer';
@@ -9,9 +7,6 @@ import { HomePage } from './pages/Home';
 import 'assets/styles/index.css';
 
 const App = () => {
-  useEffect(() => {
-    getWeatherInCity('Wroclaw');
-  }, []);
   return (
     <Router>
       <Navbar />
