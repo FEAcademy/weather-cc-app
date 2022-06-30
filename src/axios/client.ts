f95922eefaaa1b09932e9d1dfa55d5e25d7aeeb6
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-06-30 12:48:37 +0200

    delete unneeded addings

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -1,10 +1,9 @@
 import { default as axiosBase } from 'axios';
 
 const axios = axiosBase.create({
-  baseURL: 'http://api.weatherapi.com/v1',
+  baseURL: process.env.REACT_APP_WEATHER_API_URL,
   params: {
     key: process.env.REACT_APP_WEATHER_API_KEY,
-    aqi: 'yes',
     lang: 'pl',
   },
 });
