fce3fdbc49b8f420dc94dea320ce4dc23a6e1d49
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-06-30 10:59:53 +0200

    Add msw

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import App from 'App';
 import { render, screen } from 'theme/theme-test-utils';
 import { FooterTestIds } from './components/Footer/FooterTestIds';
 import { NavbarTestIds } from './components/Navbar/NavbarTestIds';
+import { handlers } from './mocks/handlers';
+import { server } from './mocks/server';
 
 describe('App', () => {
   it('should render navbar', () => {
@@ -26,5 +28,16 @@ describe('App', () => {
     const footer = screen.getByTestId(FooterTestIds.Footer);
 
     expect(footer).toBeInTheDocument();
+  });
+
+  it('msw test', () => {
+    server.use(handlers[0]);
+
+    render(<App />);
+
+    setTimeout(() => {
+      const city = screen.getByTestId('city');
+      expect(city).toHaveTextContent(/London/i);
+    }, 5000);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { IWeatherData } from 'utils/IWeatherData';
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { PageContainer } from './components/PageContainer';
@@ -6,9 +8,37 @@ import { HomePage } from './pages/Home';
 import 'assets/styles/index.css';
 
 const App = () => {
+  const [weatherData, setWeatherData] = React.useState<IWeatherData | undefined>(undefined);
+  const [errorState, setErrorState] = React.useState<string | undefined>(undefined);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&aqi=yes&q=xyz`,
+      );
+
+      if (response.status !== 200) {
+        const data = await response.json();
+        console.log(data.error.message);
+        throw new Error(data.error.message);
+      }
+
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error: any) {
+      setErrorState(error.message);
+    }
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Router>
       <Navbar />
+      {weatherData?.location !== undefined && <p data-testid="city">{weatherData.location.name}</p>}
+      {errorState !== undefined && <p data-testid="error">{errorState}</p>}
       <PageContainer>
         <Routes>
           <Route path="/" element={<HomePage />} />
