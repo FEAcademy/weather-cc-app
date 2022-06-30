7e99cd324df3a508d296114b04b3f391e54a2cb4
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-06-30 12:14:20 +0200

    base error handling done

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,40 @@
 import { rest } from 'msw';
 
+const weatherApiUrl = 'http://api.weatherapi.com/v1/current.json';
+
 const handlers = [
-  rest.get('http://api.weatherapi.com/v1/current.json', (req, res, ctx) => {
+  rest.get(weatherApiUrl, (req, res, ctx) => {
+    const key = req.url.searchParams.get('key');
+    const isAuhtenticated = key === process.env.REACT_APP_WEATER_API_KEY;
+
+    const q = req.url.searchParams.get('q');
+    const isBadRequest = q === '';
+
+    const isResultFound = q === 'xyz';
+
+    if (!isAuhtenticated)
+      return res(
+        ctx.status(403),
+        ctx.json({
+          message: 'Not Authenticated',
+        }),
+      );
+    if (isBadRequest) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: 'Bad request',
+        }),
+      );
+    }
+    if (isResultFound) {
+      return res(
+        ctx.status(404),
+        ctx.json({
+          message: 'Not Found',
+        }),
+      );
+    }
     return res(ctx.status(200), ctx.json({ temp: 20 }));
   }),
 ];
