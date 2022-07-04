e5c7763e4632d138e1ba80b82f9ce5bcdb88a669
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-04 11:42:03 +0200

    Correct caching key in useQuery

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,20 +1,22 @@
 import { getWeatherInCity } from 'axios/queries';
 import { WeatherSuccessResponse } from 'mocks/mockData';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { Box } from 'components/Box';
 import { Title } from 'components/Title';
 import { HomePageTestIds } from './HomePageTestIds';
 
 const HomePage = () => {
-  const { data, isError, error, status, isLoading } = useQuery<WeatherSuccessResponse>('weather-data', async () => {
-    return await getWeatherInCity('Wroclaw');
-  });
+  const [cityName] = useState('Wroclaw');
+  const { data, isError, error, status, isLoading } = useQuery<WeatherSuccessResponse>(['weather-data', cityName], () =>
+    getWeatherInCity(cityName),
+  );
 
   console.log({ status, data: data, isLoading, isError, error });
 
   if (isError) {
     console.log('isError ', isError);
-    return <p>Errorek</p>;
+    return <p>Error</p>;
   }
 
   return (
