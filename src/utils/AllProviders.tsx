6800e486154a066938b998c9a86e50bfa57f0021
AgataLudwiczynska-CC <a.ludwiczynska@clearcode.cc>
2022-07-05 11:34:32 +0200

    FEA-40-Add TestProviders
    Co-authored-by: darya-karpovich-cc

--- src/utils/AllProviders.tsx
+++ src/utils/AllProviders.tsx
@@ -1,13 +1,26 @@
 import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ThemeProvider } from '../theme/ThemeProvider';
+import { ThemeProvider } from 'theme/ThemeProvider';
 
 const queryClient = new QueryClient();
+const testClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
-const AllProviders = ({ children }: PropsWithChildren) => (
-  <QueryClientProvider client={queryClient}>
+type Props = {
+  client?: QueryClient;
+};
+
+const AllProviders = ({ children, client = queryClient }: PropsWithChildren<Props>) => (
+  <QueryClientProvider client={client}>
     <ThemeProvider>{children}</ThemeProvider>
   </QueryClientProvider>
 );
 
-export { AllProviders };
+const TestProviders = ({ children }: PropsWithChildren) => <AllProviders client={testClient}>{children}</AllProviders>;
+
+export { AllProviders, TestProviders };
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,9 +1,9 @@
 import { render, RenderOptions } from '@testing-library/react';
 import { ReactElement } from 'react';
-import { AllProviders } from './AllProviders';
+import { TestProviders } from './AllProviders';
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: AllProviders, ...options });
+  render(ui, { wrapper: TestProviders, ...options });
 
 export * from '@testing-library/react';
 export { customRender as render };
