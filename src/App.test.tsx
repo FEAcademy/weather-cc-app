b47e59948da68bb27fb99bad6da8d32c381acc50
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-06 14:54:48 +0200

    Add test about home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import App from 'App';
 import { render, screen } from 'test-utils';
 import { FooterTestIds } from './components/Footer/FooterTestIds';
 import { NavbarTestIds } from './components/Navbar/NavbarTestIds';
+import { TemperatureWidgetTestIds } from './components/TemperatureWidget/TemperatureWidgetTestIds';
 
 describe('App', () => {
   it('should render navbar', () => {
@@ -26,5 +27,13 @@ describe('App', () => {
     const footer = screen.getByTestId(FooterTestIds.Footer);
 
     expect(footer).toBeInTheDocument();
+  });
+
+  it('should render home page', () => {
+    render(<App />);
+
+    const homePage = screen.getByTestId(TemperatureWidgetTestIds.Container);
+
+    expect(homePage).toBeInTheDocument();
   });
 });
