269b804fb8da7f6b2af6ab3feed5f08e8e13b532
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-06 11:20:57 +0200

    Remove unnecessary code

--- src/components/TemperatureBox/TemperatureBox.styled.ts
+++ src/components/TemperatureBox/TemperatureBox.styled.ts
@@ -5,42 +5,6 @@ const Container = styled.div`
   width: 368px;
   height: 200px;
   align-items: center;
-
-  div:nth-child(1) {
-    padding-right: 17px;
-  }
-
-  img {
-    width: 126px;
-    height: 123px;
-    display: inline-block;
-    margin-left: 14px;
-  }
-
-  p {
-    color: ${({ theme }) => theme.colors.text};
-    font-weight: 300;
-  }
-
-  p:nth-child(1),
-  p:nth-child(3) {
-    font-size: 20px;
-
-    span {
-      font-size: 15px;
-      font-weight: 100;
-    }
-  }
-
-  p:nth-child(2) {
-    font-size: 96px;
-    margin-bottom: -10px;
-
-    span {
-      font-size: 48px;
-      font-weight: 100;
-    }
-  }
 `;
 
 export { Container };
--- src/components/TemperatureBox/TemperatureBox.tsx
+++ src/components/TemperatureBox/TemperatureBox.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import rain from './rain.png';
 import { Container } from './TemperatureBox.styled';
 
 const TemperatureBox = () => {
-  return (
-    <Container>
-      <div>
-        <img src={rain} />
-      </div>
-      <div>
-        <p>Partly cloudy</p>
-        <p>
-          16<span>&#8451;</span>
-        </p>
-        <p>
-          feels like 16<span>&#8451;</span>
-        </p>
-      </div>
-    </Container>
-  );
+  return <Container></Container>;
 };
 
 export { TemperatureBox };
