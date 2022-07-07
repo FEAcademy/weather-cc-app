6cf837fe3207e7d851683509719fb82055e8c2c6
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-07-07 11:35:10 +0200

    Add props interface

--- src/components/TemperatureWidget/TemperatureWidget.tsx
+++ src/components/TemperatureWidget/TemperatureWidget.tsx
@@ -1,7 +1,11 @@
 import { Container, Icon } from './TemperatureWidget.styled';
 import { TemperatureWidgetTestIds } from './TemperatureWidgetTestIds';
 
-const TemperatureWidget = ({ icon }: { icon: string }) => {
+interface Props {
+  icon: string;
+}
+
+const TemperatureWidget = ({ icon }: Props) => {
   return (
     <Container data-testid={TemperatureWidgetTestIds.Container}>
       <Icon src={icon} alt="Weather icon" />
