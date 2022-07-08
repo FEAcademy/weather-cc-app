3b7d5bdc7e06d1b3f03d5797f85965cd12cb16a0
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-07-08 13:16:58 +0200

    changed homepage data

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <div data-testid={HomePageTestIds.HomePage}>
       <WidgetWrapper>
-        {data && <TemperatureWidget icon={data?.current.condition.icon!} description={data?.current.condition.text!} />}
+        {data && <TemperatureWidget icon={data.current.condition.icon!} description={data.current.condition.text!} />}
       </WidgetWrapper>
     </div>
   );
