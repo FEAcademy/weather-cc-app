710aa2bf7c6ff4942fc9ed5dec4fb7b9ed9baf29
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-08 12:37:34 +0200

    Change props, conditional rendering and tests

--- src/components/TemperatureWidget/TemperatureWidget.tsx
+++ src/components/TemperatureWidget/TemperatureWidget.tsx
@@ -2,14 +2,14 @@ import { Container, WeatherDescription, Icon } from './TemperatureWidget.styled'
 import { TemperatureWidgetTestIds } from './TemperatureWidgetTestIds';
 
 interface Props {
-  icon?: string;
-  description?: string;
+  icon: string;
+  description: string;
 }
 
 const TemperatureWidget = ({ icon, description }: Props) => {
   return (
     <Container data-testid={TemperatureWidgetTestIds.Container}>
-      {icon && <Icon src={icon} alt="Weather widget icon" />}
+      <Icon src={icon} alt="Weather widget icon" />
       <div>
         <WeatherDescription>{description}</WeatherDescription>
       </div>
--- src/pages/Home/HomePage.test.tsx
+++ src/pages/Home/HomePage.test.tsx
@@ -4,23 +4,19 @@ import { TemperatureWidgetTestIds } from 'components/TemperatureWidget';
 import { HomePage } from './HomePage';
 
 describe('Home page', () => {
-  it('should render temperature widget', () => {
+  it('should render temperature widget', async () => {
     render(<HomePage />);
 
-    expect(screen.getByTestId(TemperatureWidgetTestIds.Container)).toBeInTheDocument();
+    expect(await screen.findByTestId(TemperatureWidgetTestIds.Container)).toBeInTheDocument();
   });
 
-  it('should contain weather description', async () => {
+  it('should render temperature widget content properly', async () => {
     render(<HomePage />);
+
     const description = await screen.findByText(/Sunny/i);
-
-    expect(description).toBeInTheDocument();
-  });
-
-  it('should render weather icon with correct src', async () => {
-    render(<HomePage />);
     const weatherIcon = await screen.findByAltText('Weather widget icon');
 
+    expect(description).toBeInTheDocument();
     expect(weatherIcon).toHaveAttribute('src', weatherSuccessResponse.current.condition.icon);
   });
 });
