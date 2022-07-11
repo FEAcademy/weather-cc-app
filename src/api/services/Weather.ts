740b5c1110691c6f6b68217f817e6ba23c21f883
Kamil Borecki CC <95024445+kamil-borecki-cc@users.noreply.github.com>
2022-07-11 08:58:30 +0200

    Moving weather interface to models catalog and modifying eslint config (#50)
    * Moving weather interface to models catalog and modifying eslint config
    
    * changing indent to 2

--- src/api/services/Weather.ts
+++ src/api/services/Weather.ts
@@ -1,11 +1,11 @@
-import { WeatherSuccessResponse } from 'mocks/mockData';
 import { useQuery } from 'react-query';
+import { Weather } from 'models/Weather';
 import { weatherClient } from '../clients';
 
 export default {
   useCity: (cityName: string) =>
-    useQuery<WeatherSuccessResponse, Error>(['weather', cityName], async () => {
-      const res = await weatherClient.get('/current.json', {
+    useQuery<Weather, Error>(['weather', cityName], async () => {
+      const res = await weatherClient.get<Weather>('/current.json', {
         params: {
           q: cityName,
           aqi: 'yes',
