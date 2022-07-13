3707bc52b72bfea609b9126357fa3091284ceab6
lukasz-konatowski-cc <107026487+lukasz-konatowski-cc@users.noreply.github.com>
2022-07-13 08:48:25 +0200

    Feature/fea-4 (#59)

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import Weather from 'api/services/Weather';
 import { useState } from 'react';
 import { TemperatureWidget } from 'components/TemperatureWidget';
+import { WeatherInfoWidget } from 'components/WeatherInfoWidget';
 import { WidgetWrapper } from './HomePage.styled';
 import { HomePageTestIds } from './HomePageTestIds';
 
@@ -12,12 +13,22 @@ const HomePage = () => {
     <div data-testid={HomePageTestIds.HomePage}>
       <WidgetWrapper>
         {data && (
-          <TemperatureWidget
-            icon={data.current.condition.icon}
-            description={data.current.condition.text}
-            currentTemperature={data.current.temp_c}
-            feelslikeTemperature={data.current.feelslike_c}
-          />
+          <>
+            <WeatherInfoWidget
+              cloud={data.current.cloud}
+              precip={data.current.precip_mm}
+              humidity={data.current.humidity}
+              pressure={data.current.pressure_mb}
+              windSpeed={data.current.wind_kph}
+              gust={data.current.gust_kph}
+            />
+            <TemperatureWidget
+              icon={data.current.condition.icon}
+              description={data.current.condition.text}
+              currentTemperature={data.current.temp_c}
+              feelslikeTemperature={data.current.feelslike_c}
+            />
+          </>
         )}
       </WidgetWrapper>
     </div>
