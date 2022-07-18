584e094c96af604e83d3d3f0c35ce2cd7eaac2bd
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-07-18 14:30:08 +0200

    updated colors for aqi widget

--- src/components/WeatherWidget/WeatherWidget.styled.tsx
+++ src/components/WeatherWidget/WeatherWidget.styled.tsx
@@ -3,7 +3,9 @@ import { serializeDefraIndex } from 'utils/serializeDefraIndex';
 
 const Container = styled.div<{ defraIndex?: number }>`
   background: ${({ theme, defraIndex }) =>
-    !defraIndex ? theme.colors.background : theme.defraBackgrounds[serializeDefraIndex(defraIndex)]};
+    defraIndex ? theme.defraTheme.backgrounds[serializeDefraIndex(defraIndex)] : theme.colors.background};
+  color: ${({ theme, defraIndex }) =>
+    defraIndex && defraIndex >= 6 ? theme.defraTheme.colors.light : theme.defraTheme.colors.dark};
   box-shadow: 0px 10px 30px rgba(227, 227, 227, 0.5);
   border-radius: 20px;
   width: 368px;
@@ -14,6 +16,9 @@ const Container = styled.div<{ defraIndex?: number }>`
   grid-template-rows: repeat(3, 1fr);
   grid-row-gap: 23px;
   grid-column-gap: 16px;
+  > div {
+    border-color: inherit;
+  }
 `;
 
 export { Container };
