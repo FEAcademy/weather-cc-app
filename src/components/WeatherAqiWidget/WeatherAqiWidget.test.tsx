bae8d1536c62d3745907a9e883dc2d582b4d9acc
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-07-18 09:10:00 +0200

    added pm10 info to aqi container

--- src/components/WeatherAqiWidget/WeatherAqiWidget.test.tsx
+++ src/components/WeatherAqiWidget/WeatherAqiWidget.test.tsx
@@ -1,13 +1,22 @@
+import { weatherSuccessResponse } from 'mocks/mockData';
 import { render, screen } from 'utils';
 import { WeatherAqiWidget } from './WeatherAqiWidget';
 import { WeatherAqiWidgetTestIds } from './WeatherAqiWidgetTestIds';
 
 describe('WeatherAqiWidget', () => {
   it('should render', () => {
-    render(<WeatherAqiWidget defraIndex={2} />);
+    render(<WeatherAqiWidget defraIndex={2} pm10={weatherSuccessResponse.current.air_quality.pm10} />);
 
     const container = screen.getByTestId(WeatherAqiWidgetTestIds.Container);
 
     expect(container).toBeInTheDocument();
+  });
+
+  it('should render proper air quality data', () => {
+    render(<WeatherAqiWidget defraIndex={2} pm10={weatherSuccessResponse.current.air_quality.pm10} />);
+
+    const pm10 = screen.getByText(/7 µg\/m3/i);
+
+    expect(pm10).toBeInTheDocument();
   });
 });
--- src/components/WeatherAqiWidget/WeatherAqiWidget.tsx
+++ src/components/WeatherAqiWidget/WeatherAqiWidget.tsx
@@ -3,10 +3,16 @@ import { WeatherAqiWidgetTestIds } from './WeatherAqiWidgetTestIds';
 
 interface Props {
   defraIndex: number;
+  pm10: number;
 }
 
-const WeatherAqiWidget = ({ defraIndex }: Props) => {
-  return <WeatherWidget defraIndex={defraIndex} data-testid={WeatherAqiWidgetTestIds.Container}></WeatherWidget>;
+const WeatherAqiWidget = ({ defraIndex, pm10 }: Props) => {
+  const roundedPm10 = Math.round(pm10);
+  return (
+    <WeatherWidget defraIndex={defraIndex} data-testid={WeatherAqiWidgetTestIds.Container}>
+      <WeatherWidget.DataItem label={'PM 10:'} value={roundedPm10 + ' µg/m3'} />
+    </WeatherWidget>
+  );
 };
 
 export { WeatherAqiWidget };
