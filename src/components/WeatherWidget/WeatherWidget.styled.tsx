0eb8762a8af5a3dbd057ad33e3e2109d5db6c2a8
lukasz-konatowski-cc <l.konatowski@clearcode.cc>
2022-07-19 11:15:20 +0200

    changed condition to display text color properly

--- src/components/WeatherWidget/WeatherWidget.styled.tsx
+++ src/components/WeatherWidget/WeatherWidget.styled.tsx
@@ -5,7 +5,7 @@ const Container = styled.div<{ defraIndex?: number }>`
   background: ${({ theme, defraIndex }) =>
     defraIndex ? theme.defraTheme.backgrounds[serializeDefraIndex(defraIndex)] : theme.colors.background};
   color: ${({ theme, defraIndex }) =>
-    defraIndex && defraIndex >= 6 ? theme.defraTheme.colors.light : theme.defraTheme.colors.dark};
+    defraIndex && defraIndex > 6 ? theme.defraTheme.colors.light : theme.defraTheme.colors.dark};
   box-shadow: 0px 10px 30px rgba(227, 227, 227, 0.5);
   border-radius: 20px;
   width: 368px;
