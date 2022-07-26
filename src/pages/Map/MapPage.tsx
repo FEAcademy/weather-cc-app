138fcac7a261ed6e79849afd3369d350563d0fdd
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-26 12:47:14 +0200

    Delete stashed changes - mistake

--- src/pages/Map/MapPage.tsx
+++ src/pages/Map/MapPage.tsx
@@ -1,15 +1,6 @@
-import { TileLayer, useMapEvents } from 'react-leaflet';
+import { TileLayer } from 'react-leaflet';
 import { StyledMapContainer, MapWrapper } from './MapPage.styled';
 import { MapPageTestIds } from './MapPageTestIds';
-
-function DataComponent() {
-  const map = useMapEvents({
-    click: () => {
-      map.locate();
-    },
-  });
-  return null;
-}
 
 const MapPage = () => {
   const localization = { long: 17.038, lat: 51.107 };
@@ -22,7 +13,6 @@ const MapPage = () => {
         zoomControl={false}
         scrollWheelZoom={true}
       >
-        <DataComponent />
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </StyledMapContainer>
     </MapWrapper>
