fb38b1970ab06e08efeecc4160409ea3d19ce452
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-07-27 12:00:55 +0200

    Add restoring mocks

--- src/pages/Home/components/GeolocationButton/GeolocationButton.test.tsx
+++ src/pages/Home/components/GeolocationButton/GeolocationButton.test.tsx
@@ -4,6 +4,10 @@ import { GeolocationButton } from './GeolocationButton';
 import { GeolocationButtonTestIds } from './GeolocationButtonTestIds';
 
 describe('GeolocationButton', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should call getLocation on click', async () => {
     const getLocation = jest.fn();
     jest.spyOn(geoLocationHooks, 'useGeoLocation').mockReturnValue([null, getLocation]);
