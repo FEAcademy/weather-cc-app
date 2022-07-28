5f304686a36efb473ea31426a651267ec91457f3
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-07-28 14:11:54 +0200

    change fireEvent to userEvent

--- src/pages/Home/components/GeolocationButton/GeolocationButton.test.tsx
+++ src/pages/Home/components/GeolocationButton/GeolocationButton.test.tsx
@@ -1,7 +1,8 @@
+import userEvent from '@testing-library/user-event';
 import * as geoLocationHooks from 'hooks/useGeoLocation';
 import { positionData } from 'mocks/mockData';
 import { mockNavigatorGeolocation } from 'test-utils';
-import { render, screen, fireEvent } from 'utils';
+import { render, screen } from 'utils';
 import { GeolocationButton } from './GeolocationButton';
 import { GeolocationButtonTestIds } from './GeolocationButtonTestIds';
 
@@ -11,18 +12,20 @@ describe('GeolocationButton', () => {
   });
 
   it('should call getLocation on click', async () => {
+    const user = userEvent.setup();
     const getLocation = jest.fn();
     const updateSavedLocation = jest.fn();
     jest.spyOn(geoLocationHooks, 'useGeoLocation').mockReturnValue([null, getLocation]);
 
     render(<GeolocationButton updateSavedLocation={updateSavedLocation} />);
 
-    fireEvent.click(screen.getByTestId(GeolocationButtonTestIds.Button));
+    await user.click(screen.getByTestId(GeolocationButtonTestIds.Button));
 
     expect(getLocation).toHaveBeenCalledTimes(1);
   });
 
   it('should call updateSavedLocation on location change', async () => {
+    const user = userEvent.setup();
     const updateSavedLocation = jest.fn();
 
     const { getCurrentPositionMock } = mockNavigatorGeolocation();
@@ -31,7 +34,7 @@ describe('GeolocationButton', () => {
 
     render(<GeolocationButton updateSavedLocation={updateSavedLocation} />);
 
-    fireEvent.click(screen.getByTestId(GeolocationButtonTestIds.Button));
+    await user.click(screen.getByTestId(GeolocationButtonTestIds.Button));
 
     expect(updateSavedLocation).toHaveBeenCalledWith(
       `${positionData.coords.latitude},${positionData.coords.longitude}`,
