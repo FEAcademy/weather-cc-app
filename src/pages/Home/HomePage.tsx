185c06e76625824889d12f0feda717277ac5884a
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-07-29 09:42:45 +0200

    Change props passed to GeolocationButton

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -62,7 +62,7 @@ const HomePage = () => {
     <Container data-testid={HomePageTestIds.HomePage}>
       <SelectContainer>
         <AutocompleteInput setSavedLocation={setSavedLocation} savedLocation={savedLocation || ''} />
-        <GeolocationButton updateSavedLocation={(location) => setSavedLocation(location)} />
+        <GeolocationButton updateSavedLocation={setSavedLocation} />
       </SelectContainer>
       <WidgetWrapper data-testid={HomePageTestIds.Widgets}>{renderContent()}</WidgetWrapper>
     </Container>
--- src/pages/Home/components/GeolocationButton/GeolocationButton.tsx
+++ src/pages/Home/components/GeolocationButton/GeolocationButton.tsx
@@ -1,4 +1,5 @@
 import { useGeoLocation } from 'hooks/useGeoLocation';
+import React from 'react';
 import { useEffect } from 'react';
 import { MapPin } from 'react-feather';
 import { serializeCoordinates } from 'utils/serializeCoordinates';
@@ -6,7 +7,7 @@ import { Button } from './GeolocationButton.styled';
 import { GeolocationButtonTestIds } from './GeolocationButtonTestIds';
 
 interface Props {
-  updateSavedLocation: (location: string) => void;
+  updateSavedLocation: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const GeolocationButton = ({ updateSavedLocation }: Props) => {
