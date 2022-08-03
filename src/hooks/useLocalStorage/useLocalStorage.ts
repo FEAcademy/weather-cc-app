8e2838dea6d7f7d791f0b130a8a849cae05b2c82
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-08-03 12:45:18 +0200

    removed typo

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -4,7 +4,7 @@ const getItem = (key: string): string | null => localStorage.getItem(key);
 
 const setItem = (key: string, value: string) => localStorage.setItem(key, value);
 
-const useLocalStorage = (key: string): [string | null, (key: string)=> void] => {
+const useLocalStorage = (key: string): [string | null, (value: string) => void] => {
   const [localValue, setLocalValue] = useState<string | null>(() => getItem(key));
 
   useEffect(() => {
