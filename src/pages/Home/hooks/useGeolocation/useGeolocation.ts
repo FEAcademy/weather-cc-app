77fd39c11f2b681e8df1c2310d48d681b68c3bd2
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-08-04 11:59:05 +0200

    fix geolocation buttom

--- src/pages/Home/hooks/useGeolocation/useGeolocation.ts
+++ src/pages/Home/hooks/useGeolocation/useGeolocation.ts
@@ -1,24 +1,24 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Coordinates } from 'models/Coordinates';
 import { Geolocation } from 'models/Geolocation';
 
 const useGeolocation = (): [Coordinates | null, () => void] => {
   const [geoLocation, setGeoLocation] = useState<Coordinates | null>(null);
 
-  const onSuccess = (geolocation: Geolocation) => {
+  const onSuccess = useCallback((geolocation: Geolocation) => {
     setGeoLocation({
       latitude: geolocation.coords.latitude,
       longitude: geolocation.coords.longitude,
     });
-  };
+  }, []);
 
-  const onError = () => {
+  const onError = useCallback(() => {
     setGeoLocation(null);
-  };
+  }, []);
 
-  const getLocation = () => {
+  const getLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  };
+  }, [onError, onSuccess]);
 
   return [geoLocation, getLocation];
 };
