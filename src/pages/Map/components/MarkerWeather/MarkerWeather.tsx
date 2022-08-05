8a92faf3415a93cd252b33fe3c23f95ecaab700f
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-08-05 09:47:33 +0200

    fix shifted tooltip position

--- src/pages/Map/components/MarkerWeather/MarkerWeather.tsx
+++ src/pages/Map/components/MarkerWeather/MarkerWeather.tsx
@@ -1,9 +1,16 @@
 import Weather from 'api/services/Weather';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import { Marker } from 'react-leaflet';
 import { Content, WeatherIcon, Temperature, City } from './MarkerWeather.styled';
 import { MarkerWeatherLoader } from './MarkerWeatherLoader';
 import { MarkerWeatherTestIds } from './MarkerWeatherTestIds';
 
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
 interface Props {
   pos: [number, number];
 }
@@ -32,7 +39,7 @@ const MarkerWeather = ({ pos }: Props) => {
 
   return (
     <div data-testid={MarkerWeatherTestIds.Container}>
-      <Marker position={pos} opacity={0}>
+      <Marker icon={DefaultIcon} position={pos} opacity={0}>
         <Content permanent={true} direction="top">
           {renderContent()}
         </Content>
