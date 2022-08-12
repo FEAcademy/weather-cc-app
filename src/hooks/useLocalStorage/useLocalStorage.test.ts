4d587c6a6ac8df3695b4cf6fee209ef396042588
adrian-konopnicki-CC <a.konopnicki@clearcode.cc>
2022-08-12 08:45:30 +0200

    Test passing different types to useLocalStorage

--- src/hooks/useLocalStorage/useLocalStorage.test.ts
+++ src/hooks/useLocalStorage/useLocalStorage.test.ts
@@ -10,6 +10,33 @@ describe('useLocalStorage', () => {
     act(() => {
       result.current[1](newValue);
     });
+
+    expect(result.current[0]).toEqual(newValue);
+    expect(JSON.parse(localStorage.getItem(key) || 'null')).toEqual(newValue);
+  });
+
+  it('should save array item to local storage', () => {
+    const key = 'current_location';
+    const newValue = ['Warszawa', 'Pcim Dolny'];
+    const { result } = renderHook(() => useLocalStorage<string[]>(key));
+
+    act(() => {
+      result.current[1](newValue);
+    });
+
+    expect(result.current[0]).toEqual(newValue);
+    expect(JSON.parse(localStorage.getItem(key) || 'null')).toEqual(newValue);
+  });
+
+  it('should save objet to local storage', () => {
+    const key = 'current_location';
+    const newValue = { value1: 'Warszawa', value2: 12 };
+    const { result } = renderHook(() => useLocalStorage<{ value1: string; value2: number }>(key));
+
+    act(() => {
+      result.current[1](newValue);
+    });
+
     expect(result.current[0]).toEqual(newValue);
     expect(JSON.parse(localStorage.getItem(key) || 'null')).toEqual(newValue);
   });
