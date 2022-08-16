bf71d46e196339d9d51f9ac297bec68357619a85
Klaudia Błażyczek <k.blazyczek@clearcode.cc>
2022-08-16 10:11:24 +0200

    Add test for loaders

--- src/pages/City/components/NearestCitiesWeatherWidget/NearestCitiesWeatherWidget.test.tsx
+++ src/pages/City/components/NearestCitiesWeatherWidget/NearestCitiesWeatherWidget.test.tsx
@@ -1,13 +1,22 @@
 import { render, screen } from 'test-utils';
+import { CityWeatherShortcutLoaderTestIds } from 'components/CityWeatherShortcut/components/CityWeatherShortcutLoader';
 import { NearestCitiesWeatherWidget } from './NearestCitiesWeatherWidget';
 import { NearestCitiesWeatherWidgetTestIds } from './NearestCitiesWeatherWidgetTestIds';
 
 describe('NearestCitiesWeatherWidget', () => {
-  it('should render', async () => {
+  it('should render', () => {
     render(<NearestCitiesWeatherWidget loading={false} cityName={undefined} />);
 
     const widget = screen.getByTestId(NearestCitiesWeatherWidgetTestIds.Widget);
 
     expect(widget).toBeInTheDocument();
+  });
+
+  it('should loadrs', async () => {
+    render(<NearestCitiesWeatherWidget loading={true} cityName={undefined} />);
+
+    const loaders = await screen.findAllByTestId(CityWeatherShortcutLoaderTestIds.Loader);
+
+    expect(loaders.length).toBe(6);
   });
 });
