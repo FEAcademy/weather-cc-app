b6f903d77504ea6fcb39fac71c884833f0de7a1f
Jakub Łukaszewski <j.lukaszewski@clearcode.cc>
2022-08-18 11:04:56 +0200

    change debounce time

--- src/pages/Map/components/PlaceMarkers/PlaceMarkers.tsx
+++ src/pages/Map/components/PlaceMarkers/PlaceMarkers.tsx
@@ -29,7 +29,7 @@ const PlaceMarkers = ({ boundsCoordinates, zoom }: Props) => {
       boundsCoordinates,
       zoom,
     });
-  }, 2000);
+  }, 1000);
 
   const map = useMapEvents({
     zoomend: () => setMapPositionDebounce(map),
