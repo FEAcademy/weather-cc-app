3d297e3f718585f04057444e30db58117d489249
darya-karpovich-cc <107031135+darya-karpovich-cc@users.noreply.github.com>
2022-08-22 10:00:40 +0200

    FEA-137 Added redirect to map page if city is not found (#142)
    * added redirect to map page if city is not found
    
    * moved test from app to city page
    
    Co-authored-by: Darya Karpovich <d.kaprovich@clearcode.cc>

--- src/pages/City/CityPage.tsx
+++ src/pages/City/CityPage.tsx
@@ -1,6 +1,7 @@
 import Weather from 'api/services/Weather';
-import { useMemo } from 'react';
-import { NavigateOptions, useLocation, useParams } from 'react-router-dom';
+import { Paths } from 'enums/Paths';
+import { useEffect, useMemo } from 'react';
+import { NavigateOptions, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { convertSpecialCharacters } from 'utils/convertSpecialCharacters';
 import { PageContentContainer } from 'components/PageContentContainer';
 import { WidgetWrapper } from 'components/WidgetWrapper';
@@ -13,12 +14,18 @@ type Params = {
 };
 
 const CityPage = () => {
+  const navigate = useNavigate();
   const { city } = useParams() as Params;
-  const normalizedCity = useMemo(()=> 
-    convertSpecialCharacters(city), [city]
-  );
-  const { data, isLoading } = Weather.useLocation(normalizedCity);
+  const normalizedCity = useMemo(() => convertSpecialCharacters(city), [city]);
+
+  const { data, isLoading, isError } = Weather.useLocation(normalizedCity);
   const { state }: NavigateOptions = useLocation();
+
+  useEffect(() => {
+    if (isError) {
+      navigate(Paths.Map);
+    }
+  }, [isError, navigate]);
 
   const renderCityName = () => {
     if (isLoading) {
