de1a37af9f5a5ce4b5d8acaf83f41177cc79b493
Darya Karpovich <d.kaprovich@clearcode.cc>
2022-08-25 15:03:36 +0200

    changed generateCacheKey function

--- src/utils/generateCacheKey/generateCacheKey.test.ts
+++ src/utils/generateCacheKey/generateCacheKey.test.ts
@@ -4,6 +4,6 @@ describe('generateCacheKey', () => {
   it('should return valid key', () => {
     const bounds = '(50.72167742756552,16.311950683593754,51.48993452350156,17.76351928710938)';
 
-    expect(generateCacheKey(bounds)).toMatchObject([51, 16, 51, 18]);
+    expect(generateCacheKey(bounds)).toMatchObject([50.7, 16.3, 51.4, 17.7]);
   });
 });
