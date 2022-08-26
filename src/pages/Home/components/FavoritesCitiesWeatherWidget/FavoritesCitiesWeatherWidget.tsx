61f8d8a4ee114b0720282f755afe68396d5ef923
AgataLudwiczynska-CC <a.ludwiczynska@clearcode.cc>
2022-08-26 10:20:40 +0200

    FEA-86-Obtain favorite cities from local storage

--- src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.tsx
+++ src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.tsx
@@ -1,16 +1,17 @@
 import { IconTypes } from 'enums/IconTypes';
+import { useLocalStorage } from 'hooks/useLocalStorage';
 import { CitiesWeatherWidget } from 'components/CitiesWeatherWidget';
 import { CityWeatherShortcut } from 'components/CityWeatherShortcut';
 import { FavoritesCitiesWeatherWidgetTestIds } from './FavoritesCitiesWeatherWidgetTestIds';
 
 const FavoritesCitiesWeatherWidget = () => {
-  const favoriteCities: string[] = [];
+  const [favoriteCities] = useLocalStorage<string[]>('favorites');
 
   return (
     <CitiesWeatherWidget data-testid={FavoritesCitiesWeatherWidgetTestIds.Container}>
       <CitiesWeatherWidget.Title>favorites</CitiesWeatherWidget.Title>
       <CitiesWeatherWidget.ShortcutsWrapper>
-        {favoriteCities.map((city) => (
+        {favoriteCities?.map((city) => (
           <CityWeatherShortcut key={city} cityName={city} iconType={IconTypes.Heart} />
         ))}
       </CitiesWeatherWidget.ShortcutsWrapper>
