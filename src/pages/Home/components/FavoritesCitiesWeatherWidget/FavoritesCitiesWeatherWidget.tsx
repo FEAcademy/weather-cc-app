c93d61d52eb7fa74f0bac4de293451d2e66a1884
AgataLudwiczynska-CC <a.ludwiczynska@clearcode.cc>
2022-08-26 12:59:56 +0200

    FEA-86-Obtain data from context

--- src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.tsx
+++ src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.tsx
@@ -1,17 +1,17 @@
+import { useFavorites } from 'context/favorites/FavoritesProvider';
 import { IconTypes } from 'enums/IconTypes';
-import { useLocalStorage } from 'hooks/useLocalStorage';
 import { CitiesWeatherWidget } from 'components/CitiesWeatherWidget';
 import { CityWeatherShortcut } from 'components/CityWeatherShortcut';
 import { FavoritesCitiesWeatherWidgetTestIds } from './FavoritesCitiesWeatherWidgetTestIds';
 
 const FavoritesCitiesWeatherWidget = () => {
-  const [favoriteCities] = useLocalStorage<string[]>('favorites');
+  const { state } = useFavorites();
 
   return (
     <CitiesWeatherWidget data-testid={FavoritesCitiesWeatherWidgetTestIds.Container}>
       <CitiesWeatherWidget.Title>favorites</CitiesWeatherWidget.Title>
       <CitiesWeatherWidget.ShortcutsWrapper>
-        {favoriteCities?.map((city) => (
+        {state.favorites.map((city) => (
           <CityWeatherShortcut key={city} cityName={city} iconType={IconTypes.Heart} />
         ))}
       </CitiesWeatherWidget.ShortcutsWrapper>
