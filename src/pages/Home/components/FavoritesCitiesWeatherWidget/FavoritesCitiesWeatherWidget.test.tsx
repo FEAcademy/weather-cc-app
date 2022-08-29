821f23c6c3c8cd555e0c23704d92141e303af4d2
AgataLudwiczynska-CC <a.ludwiczynska@clearcode.cc>
2022-08-29 12:48:15 +0200

    FEA-86-Add tests

--- src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.test.tsx
+++ src/pages/Home/components/FavoritesCitiesWeatherWidget/FavoritesCitiesWeatherWidget.test.tsx
@@ -1,4 +1,6 @@
+import * as favoriteHook from 'context/favorites/FavoritesProvider';
 import { render, screen } from 'test-utils';
+import { CityWeatherShortcutTestIds } from 'components/CityWeatherShortcut';
 import { FavoritesCitiesWeatherWidget } from './FavoritesCitiesWeatherWidget';
 import { FavoritesCitiesWeatherWidgetTestIds } from './FavoritesCitiesWeatherWidgetTestIds';
 
@@ -9,5 +11,19 @@ describe('FavoritesCitiesWeatherWidget', () => {
     const container = screen.getByTestId(FavoritesCitiesWeatherWidgetTestIds.Container);
 
     expect(container).toBeInTheDocument();
+  });
+  it('should display content when favoritecities array is not empty', async () => {
+    jest.spyOn(favoriteHook, 'useFavorites').mockReturnValue({
+      dispatch: jest.fn(),
+      state: { favorites: ['Wroclaw,Poland', 'Katowice,Poland'] },
+      checkIfIsFavorite: jest.fn(),
+    });
+    render(<FavoritesCitiesWeatherWidget />);
+
+    const title = screen.getByText(/favorites/i);
+    const favoriteCitiesWidgets = await screen.findAllByTestId(CityWeatherShortcutTestIds.Widget);
+
+    expect(title).toBeInTheDocument();
+    expect(favoriteCitiesWidgets.length).toBe(2);
   });
 });
