af89d7ef13b664c47280501b3fb20d5c2ca94d6f
lkonatowski <l.konatowski@clearcode.cc>
2022-08-31 20:43:08 +0200

    test

--- src/context/favorites/Actions.ts
+++ src/context/favorites/Actions.ts
@@ -1,6 +1,7 @@
 import { ActionTypes } from 'enums/ActionTypes';
 import { Action } from './models';
 
+
 const add = (payload: string): Action => ({ type: ActionTypes.ADD, payload });
 
 export { add };
